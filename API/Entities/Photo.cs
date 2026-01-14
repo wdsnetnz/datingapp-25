12040ab413a8f0218df07ecf768a21fe7534b451
Dinesh Silva <dnshsilva@gmail.com>
2026-01-14 15:38:44 +1000

    Seed the data to database in section 8

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace API.Entities;
+
+public class Photo
+{
+    public int Id { get; set; }
+    public required string Url { get; set; }
+    public string? PublicId { get; set; }
+
+    // Navigation property
+    public Member Member { get; set; } = null!;
+    public string MemberId { get; set; } = null!;
+    
+}
